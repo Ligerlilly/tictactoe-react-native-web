9d8c4cf24d35bf732424d18b2def17ef6fbe20d6
Jason Lane <quizathaderat@yahoo.com>
2016-07-31 01:39:04 -0700

    Add player list and challenge buttons

--- app/native/components/Lobby.js
+++ app/native/components/Lobby.js
@@ -2,6 +2,9 @@ import React, {
   Component,
   View,
   Text,
+  ListView,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import { connect } from 'react-redux'
 import { loadPlayers } from '../../actions'
@@ -9,18 +12,39 @@ import { loadPlayers } from '../../actions'
 export default class Lobby extends Component {
   constructor(props) {
     super(props)
+    this.renderRow = this.renderRow.bind(this)
   }
 
   componentDidMount() {
     const { loadPlayers } = this.props
-    setInterval(() => console.log('hi'), 1000)
+    loadPlayers()
+  }
+
+  renderRow(player) {
+    const { username } = this.props.session
+    const playerName = player.playerName
+    const canChallenge = username !== playerName
+    return (
+      <View>
+        <Text>{playerName}</Text>
+        {canChallenge && <TouchableOpacity style={{backgroundColor: '#ccc', marginBottom: 10}}><Text>challenge</Text></TouchableOpacity>}
+      </View>
+    )
   }
 
   render() {
+    let dataSource = null
+    if (this.props.players.length > 0) {
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.playerName !== r2.playerName})
+      dataSource = ds.cloneWithRows(this.props.players)
+    }
+
     return (
-      <View style={{flex: 1}}>
-        <Text style={{alignSelf: 'center'}}>{this.props.session.username}</Text>
-      </View>
+      <ScrollView style={{flex: 1}}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          {dataSource ? <ListView  dataSource={dataSource} renderRow={this.renderRow} /> : null}
+        </View>
+      </ScrollView>
     );
   }
 }
